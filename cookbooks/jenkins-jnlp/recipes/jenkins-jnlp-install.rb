928caf7edbf58ee3f69f6f39a1306607a172078a
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-01-11 19:55:59 +1100

    add agent and initial raw agent run - convert key to a secret later

--- cookbooks/jenkins-jnlp/recipes/jenkins-jnlp-install.rb
+++ cookbooks/jenkins-jnlp/recipes/jenkins-jnlp-install.rb
@@ -1,8 +1,14 @@
-# package node['jenkins-jnlp']['pkg_name'] do
-#   action :install
-# end
-
-directory "#{node['jenkins-jnlp']['var_path']}" do
-  recursive true
-  action :create
+execute 'apt-get-update' do
+  command "apt-get update"
 end
+
+package 'fontconfig' do
+  action :install
+end
+
+package 'openjdk-11-jre' do
+  action :install
+end
+
+execute 'run_agent' do
+  command "sudo java -jar agent.jar -jnlpUrl http://jenkins-master:9000/compu ter/agent/jenkins-agent.jnlp -secret b8526db756429715a7f220f7b0aa67713c435bdf06d002b7f4310ae7db4433ee -workDir "/var/lib/jenkins""
