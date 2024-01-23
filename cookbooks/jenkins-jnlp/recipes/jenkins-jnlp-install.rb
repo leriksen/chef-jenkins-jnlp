763f79a806ee1effcbd1a32205ab8fadd521d94d
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-01-23 13:34:00 +1100

    cntlm config file

--- cookbooks/jenkins-jnlp/recipes/jenkins-jnlp-install.rb
+++ cookbooks/jenkins-jnlp/recipes/jenkins-jnlp-install.rb
@@ -1,3 +1,8 @@
+require 'cntlm'
+
+cntlm = Cntlm.new
+
+
 execute 'package-update' do
   command "#{node['packager']} update -y --nobest"
 end
@@ -12,21 +17,25 @@ end
 
 execute 'curl_agent' do
   command 'curl -sO http://jenkins-master:8080/jnlpJars/agent.jar'
-  cwd      "#{node['jenkins-jnlp']['agent_home']}"
+  cwd      "#{node['jenkins_jnlp']['agent_home']}"
 end
 
-directory "#{node['jenkins-jnlp']['var_path']}" do
+directory "#{node['jenkins_jnlp']['data_path']}" do
+  recursive true
+end
+
+directory "#{node['jenkins_jnlp']['cntlm_path']}" do
   recursive true
 end
 
 user  'jenkins'
 group 'jenkins'
 
-template "#{node['jenkins-jnlp']['service_file']}" do
+template "#{node['jenkins_jnlp']['service_file']}" do
   source 'jenkins.agent.service.erb'
-  owner  "#{node['jenkins-jnlp']['agent_owner']}"
-  group  "#{node['jenkins-jnlp']['agent_group']}"
-  mode   "#{node['jenkins-jnlp']['agent_mode']}"
+  owner  "#{node['jenkins_jnlp']['agent_owner']}"
+  group  "#{node['jenkins_jnlp']['agent_group']}"
+  mode   "#{node['jenkins_jnlp']['agent_mode']}"
 end
 
 service 'jenkins.agent' do
@@ -49,3 +58,10 @@ chef_sleep 'pause' do
 end
 
 package 'cntlm'
+
+template "#{node['jenkins_jnlp']['cntlm_config']}" do
+  source 'cntlm.conf.erb'
+  owner  "#{node['jenkins_jnlp']['agent_owner']}"
+  group  "#{node['jenkins_jnlp']['agent_group']}"
+  mode   "#{node['jenkins_jnlp']['agent_mode']}"
+end
